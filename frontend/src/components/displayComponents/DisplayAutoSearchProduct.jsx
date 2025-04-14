30b7d7064f090b84450da25b6dbc5e88af41c423
GustavoFMarcial <gfm211101@gmail.com>
2025-04-13 22:18:48 -0300

    Modificado variavel input nos components AutoSearch

--- frontend/src/components/displayComponents/DisplayAutoSearchProduct.jsx
+++ frontend/src/components/displayComponents/DisplayAutoSearchProduct.jsx
@@ -1,10 +1,11 @@
 function DisplayAutoSearchProduct({autoSearch, input, handleClick}) {
     const productsArray = ["Ferragem", "Alumínio", "Plástico Bolha", "Cantoneira", "Abrasivo", "Puxador", "Kit box"];
+    const productSearch = input.product || "";
 
     return (
         <>
             <ul className="autosearch" style={{visibility: autoSearch ? "visible" : "hidden"}}>
-                {productsArray.sort().filter((item) => item.toLowerCase().includes(input.product.toLowerCase()))
+                {productsArray.sort().filter((item) => item.toLowerCase().includes(productSearch.toLowerCase()))
                 .map((item, index) => 
                     <textarea onClick={handleClick} key={index} value={item} name="product" readOnly>{item}</textarea>
                 )
