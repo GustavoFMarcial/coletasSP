c6bca7cb5af89e1e30bfffeff71e58c01378f1b5
GustavoFMarcial <gfm211101@gmail.com>
2025-04-15 21:41:17 -0300

    Corrigido código para deploy

--- frontend/src/components/displayComponents/DisplayAutoSearchCompany.jsx
+++ frontend/src/components/displayComponents/DisplayAutoSearchCompany.jsx
@@ -6,7 +6,7 @@ function DisplayAutoSearchCompany({autoSearch, input, handleClick}) {
 
     async function getCompanies() {
         try {
-            await axios.get("https://coletassp.onrender.com/companies");
+            const result = await axios.get("https://coletassp.onrender.com/companies");
             // const result = await axios.get("http://localhost:3000/companies");
             setCompanies(result.data);
         }
