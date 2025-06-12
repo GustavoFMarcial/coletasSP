eba597636e3149631aabd75048223c734d515b55
GustavoFMarcial <gfm211101@gmail.com>
2025-06-12 16:31:29 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   build: {
-    sourcemap: true,
-    outDir: 'dist', // <-- já é o padrão, mas bom deixar explícito
-    emptyOutDir: true
+    sourcemap: true
   },
 })
