173bd2aeb87f9339d416923fdf8bce7dcea438a6
GustavoFMarcial <gfm211101@gmail.com>
2025-06-15 23:01:51 -0300

    Alterando para VM

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ console.log({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
 });
 
